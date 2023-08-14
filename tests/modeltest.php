534581ad0bb987d5d8b67e409c589a7d1adfdc97
Shahriar <shahriar.hossain@prepmock.com>
2023-08-14 11:51:58 +0600

    feature/JAM-17 : added unit test for validateColumn with function

--- tests/modeltest.php
+++ tests/modeltest.php
@@ -6,7 +6,12 @@ use Jambura\Mvc\JamburaValidationError;
 
 class ModelTest extends TestCase
 {
-    public static function validateColumnDataProvider()
+    /**
+     * data provider for testValidateColumnRegex
+     *
+     * @return array
+     */
+    public static function validateColumnRegexDataProvider()
     {
         return [
             ['email', 'shahriar@gmail.com', null, null],
@@ -19,9 +24,9 @@ class ModelTest extends TestCase
 
     /**
      * @test
-     * @dataProvider validateColumnDataProvider
+     * @dataProvider validateColumnRegexDataProvider
      */
-    public function testValidateColumn($column, $value, $expectedException = null, $expectedExceptionMessage = null)
+    public function testValidateColumnRegex($column, $value, $expectedException = null, $expectedExceptionMessage = null)
     {
         $model = $this->getMockBuilder(Model::class)
             ->disableOriginalConstructor()
@@ -33,6 +38,66 @@ class ModelTest extends TestCase
             ->willReturn([
                 'email' => ['regex', '/^.+@.+\..+$/', 'Invalid email format']
             ]);
+
+        $modelReflection = new ReflectionClass($model);
+        $validateColumn = $modelReflection->getMethod('validateColumn');
+        $validateColumn->setAccessible(true);
+
+        try {
+            $validateColumn->invoke($model, $column, $value);
+
+            if ($expectedException !== null) {
+                $this->fail("Expected exception $expectedException was not thrown.");
+            } else {
+                $this->assertTrue(true, "No exception was thrown as expected.");
+            }
+        } catch (Exception $e) {
+            if ($expectedException === null) {
+                throw $e;
+            }
+
+            $this->assertInstanceOf($expectedException, $e, "Expected exception of type $expectedException, but got " . get_class($e));
+            if ($expectedExceptionMessage !== null) {
+                $this->assertEquals($expectedExceptionMessage, $e->getMessage(), "Exception message does not match expected.");
+            }
+        }
+    }
+
+    /**
+     * data provider for testValidateColumnFunction
+     *
+     * @return array
+     */
+    public static function validateColumnFunctionDataProvider()
+    {
+        return [
+            ['pass', '1qaz2wsx', null, null],
+            ['pass', 'JHvbs', JamburaValidationError::class, 'Invalid Password']
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider validateColumnFunctionDataProvider
+     */
+    public function testValidateColumnFunction($column, $value, $expectedException = null, $expectedExceptionMessage = null)
+    {
+        $model = $this->getMockBuilder(Model::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['validation'])
+            ->addMethods(['checkPass'])
+            ->getMock();
+
+        $model->expects($this->any())
+            ->method('validation')
+            ->willReturn([
+                'pass' => ['function', 'checkPass', 'Invalid Password']
+            ]);
+
+        $model->expects($this->any())
+            ->method('checkPass')
+            ->with($value)
+            ->willReturn(strlen($value) >= 8);
 
         $modelReflection = new ReflectionClass($model);
         $validateColumn = $modelReflection->getMethod('validateColumn');
